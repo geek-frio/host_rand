dac58ecb4118065ea2a4700c942446c795a53636
guangzefrio <guangzefrio@163.com>
2022-05-30 23:44:44 +0800

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use clap::Parser;
 use rand::prelude::*;
 use regex::Regex;
-use std::{collections::HashSet, fs::OpenOptions, io::Read, ops::Index, sync::mpsc::Receiver};
+use std::{
+    collections::HashSet,
+    fs::OpenOptions,
+    io::{BufWriter, Read, Write},
+    net::{SocketAddr, TcpStream, ToSocketAddrs},
+    ops::Index,
+    sync::mpsc::{Receiver, Sender},
+    time::{Duration, Instant},
+};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    // 每10分钟change一次绑定ip
-    #[clap(short, long, default_value = "10")]
-    rate: usize,
     #[clap(short, long)]
     host: String,
 }
@@ -33,15 +38,18 @@ fn check_comment_prefix(s: &str) -> bool {
     false
 }
 
-fn choose_one(buf: &mut Vec<String>, black_list: &mut HashSet<String>) {
+fn choose_one(buf: &mut Vec<String>, black_list: &mut HashSet<String>) -> String {
     let mut thread_rng = thread_rng();
     loop {
         let idx = thread_rng.gen_range(0..buf.len());
         let s = &buf[idx];
+        let ip = s.clone();
+        println!("random hosts is:{}", s);
         if black_list.contains(s) {
             continue;
         } else {
             uncomment_prefix(buf, idx);
+            return ip;
         }
     }
 }
@@ -56,26 +64,27 @@ fn uncomment_prefix(buf: &mut Vec<String>, idx: usize) {
                 if !flag {
                     if *a == '#' {
                         flag = true;
-                        true
-                    } else {
                         false
+                    } else {
+                        true
                     }
                 } else {
                     true
                 }
             })
             .collect::<String>();
+        println!("s is: {}", s);
         buf[idx] = s;
     }
 }
 
-fn re_select(buf: &mut Vec<String>, black_list: &mut HashSet<String>) {
+fn re_select(buf: &mut Vec<String>, black_list: &mut HashSet<String>) -> String {
     comment_all(buf);
-    choose_one(buf, black_list);
+    choose_one(buf, black_list)
 }
 
 fn group_host_file<'a>(host: &'a str, s: &'a String) -> (Vec<String>, Vec<String>) {
-    let lines = host.split('\n');
+    let lines = s.split('\n');
     let mut hosts: Vec<String> = Vec::new();
     let not_hosts: Vec<String> = lines
         .filter(|a| {
@@ -91,18 +100,23 @@ fn group_host_file<'a>(host: &'a str, s: &'a String) -> (Vec<String>, Vec<String
     (not_hosts, hosts)
 }
 
-fn select_on_event(not_hosts: Vec<&str>, hosts: Vec<&str>) {
-    todo!();
-}
-
 fn flush(not_hosts: &Vec<String>, hosts: &Vec<String>) {
-    todo!()
+    let file = OpenOptions::new()
+        .write(true)
+        .open("/etc/hosts")
+        .expect("打开/etc/hosts文件写入失败");
+    let chain = not_hosts.into_iter().chain(hosts.into_iter());
+    let mut buf_write = BufWriter::new(file);
+    for h in chain {
+        let _ = buf_write.write_fmt(format_args!("{}\n", h));
+    }
+    let _ = buf_write.flush();
+    println!("Host file is updated!");
 }
 
 fn read_hosts_content() -> String {
     let mut file = OpenOptions::new()
         .read(true)
-        .write(true)
         .open("/etc/hosts")
         .expect("编辑 /etc/hosts文件 失败,没有权限！");
     let mut buf = String::new();
@@ -111,25 +125,147 @@ fn read_hosts_content() -> String {
     buf
 }
 
+fn watcher_processsor(send: Sender<Event>, host: String) {
+    std::thread::spawn(move || {
+        let mut counter = 0;
+        loop {
+            let ip = get_current_inuse(host.as_str());
+            match ip {
+                Some(ip) => {
+                    let (sta, cost) = try_to_start_tcp_conn(&ip);
+                    if !sta {
+                        counter += 1;
+                        if counter >= 3 {
+                            counter = 0;
+                            let _ = send.send(Event::Default(ip));
+                        } else {
+                            println!("Retrying!");
+                        }
+                        std::thread::sleep(Duration::from_secs(1));
+                        continue;
+                    } else if cost >= 500 {
+                        let mut total_cost = 0;
+                        for _ in 0..3 {
+                            let (_, cost) = try_to_start_tcp_conn(&ip);
+                            total_cost += cost;
+                        }
+                        if total_cost / 3 >= 500 {
+                            let _ = send.send(Event::Default(ip));
+                        }
+                    }
+                }
+                None => {
+                    println!("Host is not in use, send default event....");
+                    let _ = send.send(Event::Default("".to_string()));
+                }
+            }
+            std::thread::sleep(Duration::from_secs(10))
+        }
+    });
+}
+
+fn try_to_start_tcp_conn(ip: &str) -> (bool, u128) {
+    let conn_str = format!("{}:443", ip);
+    let current = Instant::now();
+    let sock_addr = conn_str
+        .to_socket_addrs()
+        .unwrap()
+        .collect::<Vec<SocketAddr>>();
+    let res = TcpStream::connect_timeout(sock_addr.get(0).unwrap(), Duration::from_secs(1));
+    match res {
+        Ok(s) => {
+            println!(
+                "Tcp connect {} success, cost:{}ms",
+                ip,
+                current.elapsed().as_millis()
+            );
+            drop(s);
+            let elapsed = current.elapsed().as_millis();
+            if current.elapsed().as_millis() > 500 {
+                (false, elapsed)
+            } else {
+                (true, elapsed)
+            }
+        }
+        Err(e) => {
+            println!("Have met error in connecting..., e:{:?}", e);
+            let elapsed = current.elapsed().as_millis();
+            (false, elapsed)
+        }
+    }
+}
+
+fn get_current_inuse(host: &str) -> Option<String> {
+    let content = read_hosts_content();
+    let linest = content.split('\n');
+    let re = Regex::new(r"\d+\.\d+\.\d+\.\d+").unwrap();
+
+    for line in linest {
+        if line.contains(&host) && !line.starts_with("#") {
+            let s = re.captures(line).unwrap().get(0).unwrap();
+            return Some(s.as_str().to_string());
+        }
+    }
+    None
+}
+
 enum Event {
-    Default,
+    Default(String),
 }
 
 fn main() {
     let args = Args::parse();
+
     let (send, recv) = std::sync::mpsc::channel::<Event>();
 
-    let _ = send.send(Event::Default);
+    watcher_processsor(send, args.host.clone());
     loop {
         let event = recv.recv().unwrap();
         match event {
-            Event::Default => {
+            Event::Default(black_ip) => {
                 let content = read_hosts_content();
                 let (not_hosts, mut hosts) = group_host_file(args.host.as_str(), &content);
                 let mut black_list = HashSet::new();
-                re_select(&mut hosts, &mut black_list);
+                if black_ip.len() > 0 {
+                    black_list.insert(black_ip);
+                }
+                let ip = re_select(&mut hosts, &mut black_list);
                 flush(&not_hosts, &hosts);
+                println!("Host reselect ip is:{}", ip);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basics() {
+        let cont = read_hosts_content();
+        println!("cont:{}", cont);
+        let (not_hosts, mut hosts) = group_host_file("zuozhu163.xyz", &cont);
+        // println!("not_hosts: {:?}, hosts:{:?}", not_hosts, hosts);
+
+        comment_all(&mut hosts);
+        // for h in &not_hosts {
+        //     println!("{}", h);
+        // }
+        // for h in &hosts {
+        //     println!("{}", h);
+        // }
+
+        let mut black_list = HashSet::new();
+        choose_one(&mut hosts, &mut black_list);
+        for h in &hosts {
+            // println!("{}", h);
+        }
+        println!("aaaaa{:?}", get_current_inuse("zuozhu163.xyz"));
+    }
+
+    #[test]
+    fn test_watcher() {
+        println!("result is:{:?}", try_to_start_tcp_conn("103.21.247.112"));
     }
 }
